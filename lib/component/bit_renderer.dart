db174327809b87661cee3a56dc09caa982f66cd0
Will Currie <will@currie.id.au>
2014-11-21 07:15:51 +1100

    Use ng-model on custom bit decorator.
    Perhaps a little more idiomatic..

--- lib/component/bit_renderer.dart
+++ lib/component/bit_renderer.dart
@@ -3,42 +3,28 @@ library whatbit_byte_renderer;
 import 'package:angular/angular.dart';
 import 'dart:html';
 
-@Decorator(selector: 'bit', exportExpressions: const ['bit', 'value'])
-class BitRenderer implements AttachAware {
+@Decorator(selector: 'bit')
+class BitRenderer {
 
-  int _byteNumber;
-  int _bitNumber;
+  NgModel model;
   Element e;
-  Scope scope;
   Animate animate;
 
-  BitRenderer(this.e, this.scope, this.animate);
-
-  @NgOneWay('bit')
-  void set bitNumber(val) {
-    _bitNumber = val;
-  }
-
-  @NgOneWay('byte')
-  void set byteNumber(val) {
-    _byteNumber = val;
-  }
-
-  void render(val) {
-    e.text = val;
-  }
-
-  void attach() {
-    scope.watch("bit($_byteNumber, $_bitNumber).value", (val, priorVal) {
-      if (priorVal == null || priorVal == "") {
-        render(val);
+  BitRenderer(this.e, this.model, this.animate) {
+    model.render = (value) {
+      if (e.text == "") {
+        render(value);
       } else {
         animate.addClass(e, "faded").onCompleted.then((r) {
-          render(val);
+          render(value);
           animate.removeClass(e, "faded");
         });
       }
-    });
+    };
+  }
+
+  void render(val) {
+    e.text = val == null ? "" : val;
   }
 
 }
